18049614a01ef616be402555e93fa0d7e1bca10c
harry.r.wang@gmail.com <harry.r.wang@gmail.com@b7631ac0-c227-11dd-874b-8bacd04a0a74>
2014-07-22 13:01:19 +0000

    fixed a bug with SubmissionFileWriter

--- px-submission-core/trunk/src/main/java/uk/ac/ebi/pride/data/io/SubmissionFileWriter.java
+++ px-submission-core/trunk/src/main/java/uk/ac/ebi/pride/data/io/SubmissionFileWriter.java
@@ -348,7 +348,7 @@ public final class SubmissionFileWriter {
         if (dataFile.isUrl()) {
             url = dataFile.getUrl().toString();
             if (path.isEmpty()) {
-                path = dataFile.getUrl().getFile();
+                path = dataFile.getUrl().toString();
             }
         }
 
